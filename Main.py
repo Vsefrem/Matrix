b0012a90eded091e4eb2872a55a39849e6281dc1
Vsevolod <91877659+Vsefrem@users.noreply.github.com>
2024-06-21 22:30:24 +0300

    1

--- Main.py
+++ Main.py
@@ -1,19 +1,8 @@
 from MatrixProject import *
+# from sympy import init_session
+# init_session()
 
-# 1 пример
-# A_m = 3
-# A_n = 4
-# A = sp.Matrix([[1, -1, 2, 0],
-#                [-1, 2, -3, 1],
-#                [0, 5, -1, 1]])
-# y = sp.Matrix([[-1],
-#                [4],
-#                [3]])
-
-
-# 2 пример
-# A_m = 3
-# A_n = 4
+# 1 пример несовместная система
 # A = sp.Matrix([[4, -3, 2, -1],
 #                [3, -2, 1, -3],
 #                [5, -3, 1, -8]])
@@ -21,18 +10,201 @@ from MatrixProject import *
 #                [7],
 #                [1]])
 
+#2 пример совместная система
+# A = sp.Matrix([[1, -1, 2, 0],
+#                 [-1, 2, -3, 1],
+#                 [0, 5, -1, 1]])
+# y = sp.Matrix([[-1],
+#                 [4],
+#                [3]])
+#
 # 3 пример
-A_m = 3
-A_n = 3
-A = sp.Matrix([[1, 0, 0],
-               [0, 1, 0],
-               [0, 0, 0]])
+# A = sp.Matrix([[1, 1],
+#                 [1, 1]])
+#
+# y = sp.Matrix([[1],
+#                 [1]])
 
-y = sp.Matrix([[10],
-               [1],
-               [1]])
-
-
+# 4 пример
+# A = sp.Matrix([[1, 1],
+#                [1, 1]])
+# M = sp.Matrix([['a', 'a'],
+#                ['a', 'a']])
+#
+# 4 пример
+# A = sp.Matrix([[1, 1],
+#                 [2, 2]])
+# y = sp.Matrix([[3],
+#                 [6]])
+#
+# A = A + M
+# print(A)
+#
+#  y = sp.Matrix([[1],
+#                [1]])
+#
+#
 # A, A_m, A_n = enter_matrix_A()
 # y = enter_matrix_y(A_m)
-solution_of_matrix(A, y, A_m, A_n)
+#
+# solution_of_matrix(A, y)
+#
+# doc.append(Math(data=['A = ', pl.Matrix(A)], escape=False))
+# pinverse_with_pdf(A)
+# doc.generate_pdf("Matrix")
+#
+#
+# orthoproektor_na_obraz(A)
+#
+# ortoproektor_na_yadro(A)
+#
+#
+# A = Matrix([[1, 1],
+#                 [1, 1]])
+# y = Matrix([[1],
+#                [1]])
+# B = Matrix([[0, 0],
+#                [0, 0]])
+# z = Matrix([[1],
+#               [3]])
+#
+# norm_sol = norm_related_pseudo_sol(A, y, B, z)
+#
+# reg_sol = reg_sol(A, y, B, z)
+#
+#
+# from MatrixProject import *
+#
+#
+# norm_sol = norm_related_pseudo_sol(A, y, B, z)
+#
+# reg_sol = reg_sol(A, y, B, z)
+#
+# n = symbols('n')
+# parametrization_reg_sol(reg, n ** 2, 1 / n, 1 / n ** 4)
+#
+# Matrix(param)
+#
+#
+# lim = lim_param_resh(param)
+# Matrix(lim)
+# # G = regul_resh_with_print(A, y, B, z)
+#
+# n = sp.symbols('n')
+#
+# G = parametrization_reg_resh(G, 1/n, n**2, 1/(n*4))
+
+# sp.pprint(G)
+# A = sp.Matrix([[1, 0, 0, 0],
+#                 [0, e, 0, 0],
+#                [0, 0, 1, 0],
+#                [0, 0, 0, 0]])
+# y = sp.Matrix([[1],
+#                [1],
+#                [1],
+#                [1]])
+# B = sp.Matrix([[0, 0, 0, 0],
+#                 [0, 1, 0, 0],
+#                [0, 0, 0, 0],
+#                [0, 0, 0, 1]])
+# z = sp.Matrix([[1],
+#               [2],
+#                [3],
+#                [4]])
+
+
+# norm_svyazanoe_pseudoresh(A, y, B, z)
+
+# orthoproektor_na_yadro_sostavnogo_operatora_with_pdf(A, A)
+
+# orthoproektor_na_yadro_with_pdf(B)
+
+# doc.generate_pdf('test')
+
+
+# from MatrixProject import *
+#
+# e = symbols('e')
+# n = symbols('n')
+# r = n**2
+# a = 1/n
+# e = 1/n**4
+#
+# A = Matrix([[1+e, 1, 1],
+#             [1, 1, 1],
+#             [1, 1, 1]])
+# y = Matrix([[2],
+#             [3],
+#             [5]])
+# B = Matrix([[1, 1, 1],
+#             [1, 1, 1],
+#             [0, 0, e]])
+# z = Matrix([[1],
+#             [2],
+#             [3]])
+#
+# reg_sol(A, y, B, z, r, a)
+
+# parametrization_reg_sol(reg, n**2, 1/n, 1/n**4)
+
+# A = Matrix([[1+e, 1, 1],
+#             [1, 1, 1],
+#             [1, 1, 1]])
+# y = Matrix([[2],
+#             [3],
+#             [5]])
+# B = Matrix([[1, 1, 1],
+#             [1, 1, 1],
+#             [0, 0, e]])
+#             [3]])
+# z = Matrix([[1],
+#             [2],
+
+# e = symbols('e')
+#
+# A = Matrix([[1, 3],
+#             [2, 6],
+#             [3, 9],
+#             [4, 12]])
+# y = Matrix([[1],
+#             [1],
+#             [1],
+#             [1]])
+# B = Matrix([[0, 0],
+#             [0, 0],
+#             [0, 0],
+#             [0, 0]])
+# z = Matrix([[0],
+#             [0],
+#             [0],
+#             [0]])
+
+from MatrixProject import *
+
+# A = Matrix([[1, 1],
+#             [1, 1]])
+# y = Matrix([[1],
+#             [1]])
+# B = Matrix([[0, 0],
+#             [0, 0]])
+# z = Matrix([[1],
+#             [3]])
+
+norm_related_pseudo_sol(A, y, B, z)
+
+reg_sol(A, y, B, z)
+
+e = symbols('e')
+
+A = Matrix([[1, 1+e],
+            [1, 1]])
+y = Matrix([[1],
+            [1]])
+B = Matrix([[e, 0],
+            [0, 0]])
+z = Matrix([[1],
+            [3]])
+#
+# norm_related_pseudo_sol(A, y, B, z)
+#
+# reg_sol(A, y, B, z)
--- MatrixProject.py
+++ MatrixProject.py
@@ -1,201 +1,238 @@
-# coding: utf-8
-from math import sqrt
-import latexcompiler
-import sympy as sp
-from pdflatex import pdflatex
-from pylatex import Document, Math, Package, Center, Section
-from pylatex.utils import rm_temp_dir, NoEscape
-import pylatex as pl
+from sympy import *
 
+init_printing(use_unicode=True)
 
-sp.init_printing(use_unicode=False)
+def pinverse(matrix):
+    """
+    Функция для вычисления псевдообратной матрицы.
 
-rm_temp_dir()
-doc = Document(documentclass='article', document_options=None, fontenc=['T2A', 'T1'], lmodern=None, textcomp=None,
-               page_numbers=None, indent=True, data=None)
-doc.packages.add(Package('babel', options=['english', 'russian']))
-doc.packages.add(Package('substitutefont'))
-doc.preamble.append(NoEscape(r'\substitutefont {T2A} {/familydefault} {Tempora-TLF}'))
+    Аргументы:
+    matrix -- Матрица, экземпляр класса Matrix библиотеки SymPy
 
-
-
-def pinverse(mat):
-    # if mat_m == mat.rank():
-    print("Строки матрицы C л.н. => матрица CC^T обратима.")
-    doc.append('Строки матрицы ')
-    doc.append(Math(inline=True, data=['C'], escape=False))
-    doc.append('\xa0линейно независимы.')
-    doc.append('\nСледовательно, матрица ')
-    doc.append(Math(inline=True, data=['CC^T'], escape=False))
-    doc.append('\xa0обратима.')
-    print("Найдем псевдообратную матрицу по формуле C⁺ = C^T (CC^T)⁻¹:")
-    doc.append('\nНайдем псевдообратную матрицу по формуле ')
-    doc.append(Math(inline=True, data=['C^+ = C^T(CC^T)^{-1}'], escape=False))
-    mat = mat.T * (mat * mat.T).inv()
-    print("C⁺: ")
-    sp.pprint(mat)
-    doc.append(Math(data=['C^+ = ', pl.Matrix(mat)], escape=False))
-    return mat
-    # else:
-    #     print("Найдем псевдообратную матрицу по формуле " + sym + "⁺ = (" + sym + sym + "^T)⁻¹ " + sym + "^T :")
-    #     doc.append('Найдем псевдообратную матрицу по формуле ' + sym + '⁺ = (' + sym + sym + '^T)⁻¹ ' + sym + '^T :')
-    #     mat = (mat * mat.T).inv() * mat.T
-    #     print(sym + "⁺: ")
-    #     sp.pprint(mat)
-    #     doc.append(Math(data=[str(sym) + '⁺: ', pl.Matrix(mat)]))
-    #     return mat
-
-def sovmestnost_check(mat, mat_pinv, column):
-    print("Проверим уравнение на совместность.")
-    print("Оно должно довлетворять условию AA⁺y = y")
-    doc.append('Проверим уравнение на совместность.')
-    doc.append('Оно должно довлетворять условию: ')
-    doc.append(Math(data=["AA^+y = y"], escape=False))
-    if mat * mat_pinv * column == column:
-        doc.append(Math(data=[pl.Matrix(mat * mat_pinv * column), ' = ', pl.Matrix(column)], escape=False))
-        return True
+    Возвращаемое значение:
+    Псевдообратная матрица, экземпляр класса Matrix библиотеки SymPy
+    """
+    if matrix.shape[0] == matrix.rank():
+        matrix_pinv = matrix.T * (matrix * matrix.T).inv()
+        return matrix_pinv
     else:
-        doc.append(Math(data=[pl.Matrix(mat * mat_pinv * column), r'\neq', pl.Matrix(column)], escape=False))
-        return False
+        matrix_b, matrix_c = matrix.rank_decomposition()
+        matrix_b_size = shape(matrix_b)
+        if matrix_b_size[0] == matrix.rank():
+            temp_matrix = matrix_c
+            matrix_c = matrix_b
+            matrix_b = temp_matrix
+        matrix_c_pinv = pinverse(matrix_c)
+        matrix_b_temp = matrix_b.T * matrix_b
+        matrix_b_temp = matrix_b_temp.inv()
+        matrix_pinv = matrix_c_pinv * matrix_b_temp * matrix_b.T
+        return matrix_pinv
 
-def enter_matrix_A():
-    print("Ax=y")
-    print("Введите количество строк матрицы A:")
-    A_m = int(input())
-    print("Введите количество столбцов матрицы A:")
-    A_n = int(input())
-    A = sp.Matrix.zeros(A_m, A_n)
-    print("Заполнение матрицы A размера ", A_m, "x", A_n, ":", sep='')
-    for i in range(A_m):
-        for j in range(A_n):
-            print("A[", i + 1, ", ", j + 1, "]: ", sep='', end="")
-            temp = input()
-            if temp.isnumeric():
-                A[i, j] = float(temp)
-            else:
-                A[i, j] = temp
-    return A, A_m, A_n
+def orthoprojector_on_core(matrix):
+    """
+    Функция для вычисления ортопроектора на ядро заданной матрицы по формуле P(N(A)) = I - A^+A.
 
-def enter_matrix_y(A_m):
-    print("Заполнение вектора y длины ", A_m, ": ", end="")
-    y = sp.Matrix.zeros(A_m, 1)
-    for i in range(A_m):
-        print("y[", i + 1, "]: ", sep='', end="")
-        temp = input()
-        if temp.isnumeric():
-            y[i] = float(temp)
-        else:
-            y[i] = temp
-    return y
+    Аргументы:
+    matrix -- Матрица, экземпляр класса Matrix библиотеки SymPy
 
-def solution_of_matrix(A, y, A_m, A_n):
+    Возвращаемое значение:
+    Ортопроектор на ядро, экземпляр класса Matrix библиотеки SymPy
+    """
+    matrix = pinverse(matrix) * matrix
+    matrix = eye(matrix.shape[0]) - matrix
+    return matrix
+
+def norm_related_pseudo_sol(matrix_a, column_y, matrix_b, column_z):
+    """
+    Функция для вычисления нормального связанного псевдорешения по формуле x^*=B^+ z+(AP)^+ (y-AB^+ z),
+    а также вывода процесса вычислений на экран.
+
+    Аргументы:
+    matrix_a -- Матрица А основного уравнения, экземпляр класса Matrix библиотеки SymPy
+    column_y -- Вектор y основного уравнения, экземпляр класса Matrix библиотеки SymPy
+    matrix_b -- Матрица B дополнительного уравнения, экземпляр класса Matrix библиотеки SymPy
+    column_z -- Вектор z дополнительного уравнения, экземпляр класса Matrix библиотеки SymPy
+
+    Возвращаемое значение:
+    Нормальное связанное псевдорешение, экземпляр класса Matrix библиотеки SymPy
+    """
+    print("Найдем нормальное связанное псевдорешение задачи по формуле x^* = B⁺z+(AP)⁺+(y-AB⁺z)")
     print("A: ")
-    sp.pprint(A)
-    print()
+    pprint(matrix_a)
     print("y: ")
-    sp.pprint(y)
-    print()
-    with doc.create(Center()) as center:
-        with center.create(Section('Решение уравнения Ax = y')) as sec:
-            sec.numbering = None
-    doc.append(Math(data=['A = ', pl.Matrix(A)]))
-    doc.append(Math(data=['y = ', pl.Matrix(y)]))
+    pprint(column_y)
+    print("B: ")
+    pprint(matrix_b)
+    print("z: ")
+    pprint(column_z)
+    b_pinv = pinverse(matrix_b)
+    print("B⁺: ")
+    pprint(b_pinv)
+    orp_b = orthoprojector_on_core(matrix_b)
+    print("P: ")
+    pprint(orp_b)
+    ap_pinv = pinverse(matrix_a * orp_b)
+    print("(AP)⁺: ")
+    pprint(ap_pinv)
+    result = b_pinv * column_z + ap_pinv * (column_y - matrix_a * b_pinv * column_z)
+    print("Нормальное связанное псевдорешение: ")
+    result = expand(result)
+    result = simplify(result)
+    pprint(Matrix(result))
+    return result
 
-    if A_m == A.rank() & A_n == A.rank():
-        A_inv = A.inv()
-        x = A_inv * y
-        print("x = ")
-        doc.append(Math(data=['x = ', pl.Matrix(x)]))
-        sp.pprint(x)
-        doc.generate_pdf('MatrixFile')
-        quit()
+def reg_sol(matrix_a, column_y, matrix_b, column_z, r = None, a = None):
+    """
+    Функция для вычисления регуляризованного решения по формуле x(ra) = (aI + G_op(r)^* * G_op(or))^-1 * G_op(r)^* * g(r),
+    а также вывода хода решения.
+
+    Аргументы:
+    matrix_a -- Матрица А основного уравнения, экземпляр класса Matrix библиотеки SymPy
+    column_y -- Вектор y основного уравнения, экземпляр класса Matrix библиотеки SymPy
+    matrix_b -- Матрица B дополнительного уравнения, экземпляр класса Matrix библиотеки SymPy
+    column_z -- Вектор z дополнительного уравнения, экземпляр класса Matrix библиотеки SymPy
+    r -- значение параметра а, экземпляр класса Symbol библиотеки SymPy
+    a -- значение параметра r, экземпляр класса Symbol библиотеки SymPy
+
+    Возвращаемое значение:
+    Регуляризованное решение, экземпляр класса Matrix библиотеки SymPy
+    """
+    if r is None and a is None:
+        r, a = symbols('r a')
+    elif a is None:
+        a = symbols('a')
+    elif r is None:
+        r = symbols('r')
+
+    print("A: ")
+    pprint(matrix_a)
+    print("y: ")
+    pprint(column_y)
+    print("B: ")
+    pprint(matrix_b)
+    print("z: ")
+    pprint(column_z)
+    operator_1 = r * matrix_b.T * matrix_b + matrix_a.T * matrix_a
+    operator_2 = a * eye(operator_1.shape[0]) + operator_1
+    operator_2 = operator_2.inv()
+    result = operator_2 * (r * matrix_b.T * column_z + matrix_a.T * column_y)
+    print("Регуляризованное решение x(ra) = ")
+    result = expand(result)
+    result = simplify(result)
+    pprint(Matrix(result))
+    return result
+
+def parametrization_reg_sol (matrix, r_new = None, a_new = None, e_new = None):
+    """
+    Функция для параметризации регуляризованного решения.
+
+    Аргументы:
+    matrix -- Регуляризованное решение, экземпляр класса Matrix библиотеки SymPy
+    r_new -- значение параметра r, экземпляр класса Symbol библиотеки SymPy
+    a_new -- значение параметра а, экземпляр класса Symbol библиотеки SymPy
+    e_new -- значение параметра e, экземпляр класса Symbol библиотеки SymPy
+
+    Возвращаемое значение:
+    Параметризованное регуляризованное решение, экземпляр класса Matrix библиотеки SymPy
+    """
+    if a_new is not None:
+        a = symbols('a')
+        matrix = matrix.subs({a: a_new})
     else:
-        if A_m == A.rank():
-            A_pinv = pinverse(A)
-        else:
-            print("Построим скелетное разложение матрицы А")
-            doc.append('Построим скелетное разложение матрицы ')
-            doc.append(Math(inline=True, data=['A.'], escape=False))
-            B, C = A.rank_decomposition()
-            B_size = sp.shape(B)
-            C_size = sp.shape(C)
-            if B_size[0] == A.rank():
-                temp_matrix = C
-                C = B
-                B = temp_matrix
-            print("B = ")
-            sp.pprint(B)
-            doc.append(Math(data=['B = ', pl.Matrix(B)]))
-            print("C = ")
-            sp.pprint(C)
-            doc.append(Math(data=['C = ', pl.Matrix(C)]))
-            C_pinv = pinverse(C)
-            B_temp = B.T * B
-            B_temp = B_temp.inv()
-            print("Найдем псевдообратную матрицу A⁺ по формуле A⁺ = C⁺(B^T B)⁻¹B^T: ")
-            doc.append('\nНайдем псевдообратную матрицу ')
-            doc.append(Math(inline=True, data=['A^+'], escape=False))
-            doc.append('\xa0по формуле: ')
-            doc.append(Math(data=['A^+ = C^+(B^T B)^{-1}B^T'], escape=False))
-            A_pinv = C_pinv * B_temp * B.T
-            sp.pprint(A_pinv)
-            doc.append(Math(data=['A^+ = ', pl.Matrix(A_pinv)], escape=False))
-
-    sovmestnost = sovmestnost_check(A, A_pinv, y)
-    if sovmestnost:
-        print("Система совместна")
-        print("Мы можем представить систему в виде x = A⁺y + (E - A⁺A)q")
-        doc.append('Система совместна.')
-        doc.append('\nМы можем представить систему в виде ')
-        doc.append(Math(inline=True, data=['x = A^+y + (E - A^+A)q '], escape=False))
-        print("Общее решение системы имеет вид: ")
-        doc.append('\nОбщее решение системы имеет вид: ')
-        A_pinv_size = sp.shape(A_pinv)
-        E = sp.eye(A_pinv_size[0])
-        q = sp.Matrix(A_pinv_size[0], 1, lambda i, j: "q_" + str(i + 1))
-        x = A_pinv * y + (E - A_pinv * A) * q
-        print("x = ")
-        sp.pprint(x)
-        doc.append(Math(data=['x = ', pl.Matrix(x)]))
+        a_new = symbols('a')
+    if r_new is not None:
+        r = symbols('r')
+        matrix = matrix.subs({r: r_new})
     else:
-        print("Система не совместна")
-        print("Найдем нормальное псевдорешение уравнения: ")
-        doc.append('Система не совместна.')
-        doc.append('\nНайдем нормальное псевдорешение уравнения: ')
-        x = A_pinv * y
-        print("x⁰ = A⁺y")
-        sp.pprint(x)
-        doc.append(Math(data=['x^0 = A^+y = ', pl.Matrix(x)], escape=False))
-        print("Длина минимальной невязки равна")
-        doc.append('Длина минимальной невязки равна:')
-        temp = y - A * x
-        temp = temp.T * temp
-        nevyazka = temp[0]
-
-        try:
-            nevyazka = sp.N(sqrt(nevyazka), 7)
-        except Exception:
-            nevyazka = sp.sqrt(nevyazka)
-        print("∥y-Ax⁰∥ = ")
-        sp.pprint(nevyazka)
-        doc.append(Math(data=['\|y-Ax^0\| = ', nevyazka], escape=False))
-        x_size = sp.shape(x)
-        x_length = 0
-        for i in range(x_size[0]):
-            x_length += x[i] * x[i]
-        print("Длинна невязки вектора x⁰ равна")
-        print("∥x⁰∥ = ")
-        try:
-            x_length = sp.N(sqrt(x_length), 7)
-        except Exception:
-            x_length = sp.sqrt(x_length)
-        sp.pprint(x_length)
-        doc.append('Длинна невязки вектора ')
-        doc.append(Math(inline=True, data=['x^0'], escape=False))
-        doc.append('\xa0равна: ')
-        doc.append(Math(data=['\|x^0\| = ', x_length], escape=False))
-        doc.generate_pdf("Matrix")
+        r_new = symbols('r')
+    if e_new is not None:
+        e = symbols('e')
+        matrix = matrix.subs({e: e_new})
+    else:
+        e_new = symbols('e')
+    matrix = expand(matrix)
+    matrix = simplify(matrix)
+    print("Значения параметров:")
+    print("a = " + str(a_new))
+    print("r = " + str(r_new))
+    print("e = " + str(e_new))
+    print("Параметризованная матрица: ")
+    pprint(Matrix(matrix))
+    return matrix
 
 
 
 
+
+
+
+def orthoproektor_na_yadro_sostavnogo_operatora(matrix_a, matrix_b):
+    #P(N(Г)) = P(N(B)) * P(N(A * P(N(B))))
+    orp_b = orthoprojector_on_core(matrix_b)
+    orp_a_b = orthoprojector_on_core(matrix_a * orp_b)
+    result = orp_b * orp_a_b
+    pprint(result)
+    return result
+
+def orthoproektor_na_yadro_sostavnogo_operatora_print(matrix_a, matrix_b):
+    #P_(N(Г)) = P(N(B)) * P_(N(A * P(N(B))))
+    print("Найдем ортопроектор на ядро составного оператора.")
+    print("Матрица A: ")
+    pprint(matrix_a)
+    print("Матрица B: ")
+    pprint(matrix_b)
+    orp_b = orthoprojector_on_core(matrix_b)
+    print("Ортопроектор P на ядро матрицы B")
+    pprint(orp_b)
+    orp_a_b = orthoprojector_on_core(matrix_a * orp_b)
+    print("Ортопроектор на ядро матрицы AP: ")
+    pprint(orp_a_b)
+    result = orp_b * orp_a_b
+    print("Ортопроектор на ядро составного оператора: ")
+    pprint(result)
+    return result
+
+def lim_param_resh (matrix):
+    n = symbols('n')
+    expr = limit(matrix, n, 00)
+    pprint(expr)
+    return expr
+
+def orthoproektor_na_obraz(matrix):
+    #P(R(A)) = AA^+
+    matrix = matrix * pinverse(matrix)
+    return matrix
+
+def pinverse_print(matrix):
+    if matrix.shape[0] == matrix.rank():
+        print("Строки матрицы C л.н. => матрица CC^T обратима.")
+        print("Найдем псевдообратную матрицу по формуле C⁺ = C^T (CC^T)⁻¹:")
+        matrix = matrix.T * (matrix * matrix.T).inv()
+        print("C⁺: ")
+        Matrix(matrix)
+        return matrix
+    else:
+        print("Построим скелетное разложение матрицы А")
+        matrix_b, matrix_c = matrix.rank_decomposition()
+        matrix_b_size = shape(matrix_b)
+        if matrix_b_size[0] == matrix.rank():
+            temp_matrix = matrix_c
+            matrix_c = matrix_b
+            matrix_b = temp_matrix
+        print("B = ")
+        Matrix(matrix_b)
+        print("C = ")
+        Matrix(matrix_c)
+        matrix_c_pinv = pinverse_print(matrix_c)
+        matrix_b_temp = matrix_b.T * matrix_b
+        matrix_b_temp = matrix_b_temp.inv()
+        print("Найдем псевдообратную матрицу A⁺ по формуле A⁺ = C⁺(B^T B)⁻¹B^T: ")
+        matrix_pinv = matrix_c_pinv * matrix_b_temp * matrix_b.T
+        Matrix(matrix_pinv)
+        return matrix_pinv
+
+# def obshee_svyazanoe_with_print(matrix_a, column_y, matrix_b, column_z):
+#     #x = x^* + Qu
+#     norm_resh = norm_svyazanoe_pseudoresh_with_print(matrix_a, column_y, matrix_b, column_z)
+#     Q = orthoproektor_na_yadro_sostavnogo_operatora(matrix_a, matrix_b)
+#     x = norm_resh + Q *
